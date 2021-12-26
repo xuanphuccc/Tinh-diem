eb375cf9ceefbf3f5933aba65e047f95f3b81554
XuanPhuc <xuanphuca1k66@gmail.com>
2021-12-26 23:08:57 +0700

    fix validate

--- Du_doan/assets/js/script1.js
+++ Du_doan/assets/js/script1.js
@@ -118,13 +118,13 @@ function setDiemHKGoc () {
                 <div class="row target-row">
                     <div class="col l-12 m-12 c-12">
                         <div class="row point-wrap">
-                            <div class="col l-12 m-12 c-12 center">
+                            <div class="col l-12 m-12 c-12">
                                 <div class="label-container" id="label-point${count}">
                                     <label for="term-point${count}">Nhập điểm tổng kết học kỳ <span class="high-light">${count}</span> (hệ 4)</label>
                                 </div>
                                 <input type="number" name="" id="term-point${count}" value="">
                             </div>
-                            <div class="col l-12 m-12 c-12 center">
+                            <div class="col l-12 m-12 c-12">
                                 <div class="label-container" id="label-tc${count}">
                                     <label for="term-number${count}">Nhập tổng tín chỉ học kỳ <span class="high-light">${count}</span></label>
                                 </div>
@@ -148,13 +148,13 @@ function setDiemHKGoc () {
 function setDiemHKVaTinChi () {
     var getPointsWrap = document.querySelector('.point-wrap');
     getPointsWrap.innerHTML = `
-    <div class="col l-12 m-12 c-12 center">
+    <div class="col l-12 m-12 c-12">
         <div class="label-container" id="label-point${count}">
             <label for="term-point${count}">Nhập điểm tổng kết học kỳ <span class="high-light">${count}</span> (hệ 4)</label>
         </div>
         <input type="number" name="" id="term-point${count}">
     </div>
-    <div class="col l-12 m-12 c-12 center">
+    <div class="col l-12 m-12 c-12">
         <div class="label-container" id="label-tc${count}">
             <label for="term-number${count}">Nhập tổng tín chỉ học kỳ <span class="high-light">${count}</span></label>
         </div>
@@ -205,7 +205,7 @@ function getDiemHKVaTinChi () {
             if (diemHK < 0) {
                 labelPointWrap.innerHTML = `
                 <i style="color:#F55C47;" class="material-icons-outlined">highlight_off</i>
-                <label style="color:#F55C47;">Điểm không thể nhỏ hơn 0</label>
+                <label style="color:#F55C47;">Điểm không thể âm</label>
                 `;
             }
             if (diemHK > 4) {
@@ -251,7 +251,7 @@ function getDiemHKVaTinChi () {
 
 function DanhSach () {
     var check = getDiemHKVaTinChi();
-    console.log(check);
+
     if (check) {
         setTimeout (function() {
             var tmp = new DiemHocKy (diemHK, soTC);
@@ -281,7 +281,7 @@ function setFullTC () {
             </div>
             <div class="col l-7 m-7 c-12 target-container">
                 <div class="row target-row">
-                    <div class="col l-12 m-12 c-12 center">
+                    <div class="col l-12 m-12 c-12">
                         <div class="label-container" id="full-tc-wrap">
                             <label for="full-tc">Nhập số tín chỉ toàn khóa học</label>
                         </div>
@@ -303,6 +303,7 @@ function getFullTC () {
     var fullTCWrap = document.getElementById('full-tc-wrap');
 
     fullTc = getFull.value * 1;
+    var check = false;
 
     if (getFull.value != "") {
         if (Number.isInteger(fullTc) && fullTc > 0) {
@@ -310,6 +311,8 @@ function getFullTC () {
             <i style="color: #4AA96C;" class="material-icons-outlined">check_circle</i>
             <label style="color: #4AA96C;">Nhập thành công</label>
             `;
+
+            check = true;
             setTimeout(function() {
                 tinhDiem();
             },1000);
@@ -325,6 +328,14 @@ function getFullTC () {
             <label style="color: #F55C47;" for="full-tc">Chưa nhập thông tin</label>
         `;
     }
+
+    if(check == false) {
+        setTimeout(function() {
+            fullTCWrap.innerHTML =`
+            <label for="full-tc">Nhập số tín chỉ toàn khóa học</label>
+            `;
+        },1000);
+    }
 }
 
 function tinhDiem () {
@@ -338,98 +349,53 @@ function tinhDiem () {
     var diemHienTai = tongDaHoc/tongTCDaHoc;
     var diemCanDat = ((target * fullTc) - tongDaHoc) / (fullTc - tongTCDaHoc);
 
-    
+    inputWrap.innerHTML = `
+    <div class="target-wrap">
+        <div class="row">
+            <div class="col l-5 m-5 c-12 sticker-container">
+                <h2>Đây rồi kết quả của bạn nè</h2>
+                <div class="sticker-wrap">
+                    <img class="sticker" src="./assets/img/success.png" alt="">
+                </div>
+            </div>
+            <div class="col l-7 m-7 c-12 target-container">
+                <div class="row target-row">
+                    <div class="col l-12 m-12 c12 center" id="result-wrap">
+                        <!-- JS -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    var resultWrap = document.getElementById('result-wrap');
     if (target == 3.20) {
         if (diemHienTai < 3.20) {
-            inputWrap.innerHTML = `
-        <div class="target-wrap">
-            <div class="row">
-                <div class="col l-5 m-5 c-12 sticker-container">
-                    <h2>Đây rồi kết quả của bạn nè</h2>
-                    <div class="sticker-wrap">
-                        <img class="sticker" src="./assets/img/success.png" alt="">
-                    </div>
-                </div>
-                <div class="col l-7 m-7 c-12 target-container">
-                    <div class="row target-row">
-                        <div class="col l-12 m-12 c12 center">
-                            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
-                            <h2>Các học kỳ tiếp theo cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để đạt mục tiêu Bằng giỏi!</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
+            resultWrap.innerHTML = `
+            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
+            <h2>Các học kỳ tiếp theo cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để đạt mục tiêu Bằng giỏi!</h2>
+            `;
         } else {
-            inputWrap.innerHTML = `
-        <div class="target-wrap">
-            <div class="row">
-                <div class="col l-5 m-5 c-12 sticker-container">
-                    <h2>Đây rồi kết quả của bạn nè</h2>
-                    <div class="sticker-wrap">
-                        <img class="sticker" src="./assets/img/success.png" alt="">
-                    </div>
-                </div>
-                <div class="col l-7 m-7 c-12 target-container">
-                    <div class="row target-row">
-                        <div class="col l-12 m-12 c12 center">
-                            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
-                            <h2>Bạn đã đủ điểm để đạt Bằng giỏi</h2>
-                            <h2>Cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để duy trì mục tiêu Bằng giỏi!</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
+            resultWrap.innerHTML = `
+            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
+            <h2>Bạn đã đủ điểm để đạt Bằng giỏi</h2>
+            <h2>Cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để duy trì mục tiêu Bằng giỏi!</h2>
+            `;
         }
     }
     
     if (target == 2.50) {
         if (diemHienTai < 2.50) {
-            inputWrap.innerHTML = `
-            <div class="target-wrap">
-                <div class="row">
-                    <div class="col l-5 m-5 c-12 sticker-container">
-                        <h2>Đây rồi kết quả của bạn nè</h2>
-                        <div class="sticker-wrap">
-                            <img class="sticker" src="./assets/img/success.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col l-7 m-7 c-12 target-container">
-                        <div class="row target-row">
-                            <div class="col l-12 m-12 c12 center">
-                                <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
-                                <h2>Các học kỳ tiếp theo cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để đạt mục tiêu Bằng khá!</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `
+            resultWrap.innerHTML = `
+            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
+            <h2>Các học kỳ tiếp theo cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để đạt mục tiêu Bằng khá!</h2>
+            `;
         } else {
-            inputWrap.innerHTML = `
-            <div class="target-wrap">
-                <div class="row">
-                    <div class="col l-5 m-5 c-12 sticker-container">
-                        <h2>Đây rồi kết quả của bạn nè</h2>
-                        <div class="sticker-wrap">
-                            <img class="sticker" src="./assets/img/success.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col l-7 m-7 c-12 target-container">
-                        <div class="row target-row">
-                            <div class="col l-12 m-12 c12 center">
-                                <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
-                                <h2>Bạn đã đủ điểm đạt Bằng khá!</h2>
-                                <h2>Cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để duy trì mục tiêu Bằng khá!</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `
+            resultWrap.innerHTML = `
+            <h2>Điểm của bạn hiện tại <span class="high-light">${diemHienTai.toFixed(2)}</span></h2>
+            <h2>Bạn đã đủ điểm đạt Bằng khá!</h2>
+            <h2>Cần tối thiểu <span class="high-light">${diemCanDat.toFixed(2)}</span> điểm để duy trì mục tiêu Bằng khá!</h2>
+            `;
         }
     }
 }
