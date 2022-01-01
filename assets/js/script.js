cc05333e7159e5af1dd67152424aa7e6215d823b
XuanPhuc <xuanphuca1k66@gmail.com>
2022-01-01 20:10:35 +0700

    auto scale navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -304,9 +304,11 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    navbar.style.position = 'fixed';
   } else {
     navbar.classList.remove("sticky");
+    navbar.style.position = 'relative';
   }
 }
 
